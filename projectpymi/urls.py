c6d5febac233e6d6389a103761ed7bb46c51cf5c
Nguyen Dinh  Toan <toan.toan.dz@gmail.com>
2022-07-17 01:03:23 +0700

    make it better

--- projectpymi/urls.py
+++ projectpymi/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from xml.etree.ElementInclude import include
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path
 from app1 import views
 from django.conf.urls.static import static
