40090ed38b4f891ed020a9381a678de12de6c744
Nguyen Dinh  Toan <toan.toan.dz@gmail.com>
2022-07-17 21:19:44 +0700

    abc

--- projectpymi/settings.py
+++ projectpymi/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
 from pathlib import Path
+from django.conf import settings
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -119,6 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (
+  os.path.join(STATIC_ROOT, 'static/'),
+)
+
 
 
 
